b6130b446758f39085deba6c7572a45e65ccefec
Milad Stefan <98669288+StefaMi@users.noreply.github.com>
2025-05-26 10:24:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def login():
             logger.info(f"Login erfolgreich: {user.username} ({session['role']})")
             logger.info(f"Session gesetzt: {session}")
             flash(f"Eingeloggt als {user.username}", "success")
-            return redirect(url_for('home'))
+            return redirect(url_for('dashboard'))
         else:
             logger.warning(f"Login fehlgeschlagen für Benutzer: {username}")
             flash("Ungültiger Benutzer oder Passwort", "danger")
