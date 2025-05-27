0aed43279ff4f0fb6acbc2d920bee792a1349d92
Milad Stefan <98669288+StefaMi@users.noreply.github.com>
2025-05-27 13:58:24 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
@@ -10,7 +11,7 @@ class Role(db.Model):
     can_manage_users = db.Column(db.Boolean, default=False)
     can_assign_roles = db.Column(db.Boolean, default=False)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
